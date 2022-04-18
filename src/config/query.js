71478bd69cf7ff76c9f0352193f191cf19343f38
Muhammad Ihsan Erdiansyah <Ihsan.erdi97@gmail.com>
2022-04-19 04:26:00 +0700

    IntegrationregisLogin

--- src/config/query.js
+++ src/config/query.js
@@ -20,3 +20,21 @@ export const createBooking = gql`
     bookingBallroom(customerId: $customerId, hotelApiId: $hotelApiId, bookingDate: $bookingDate, name: $name, access_token: $accessToken, role: $role)
   }
 `;
+
+export const createCustomer = gql`
+  mutation RegisterCustomer($imageUrl: String, $email: String, $username: String, $password: String, $phoneNumber: String, $address: String) {
+    registerCustomer(imageUrl: $imageUrl, email: $email, username: $username, password: $password, phoneNumber: $phoneNumber, address: $address)
+  }
+`;
+
+export const loginCustomer = gql`
+  mutation LoginUser($email: String, $password: String) {
+    loginUser(email: $email, password: $password) {
+      msg
+      token
+      id
+      email
+      role
+    }
+  }
+`;
